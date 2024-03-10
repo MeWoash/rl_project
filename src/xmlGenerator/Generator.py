473dc178b4601fa460556df750b64cc0e299ed87
Milosz Zubala <miloszzubala@gmail.com>
2024-03-10 12:43:24 +0100

    Added Generic car creation

--- src/xmlGenerator/Generator.py
+++ src/xmlGenerator/Generator.py
@@ -5,18 +5,74 @@ import os
 import xml.etree.ElementTree as ET
 from pathlib import Path
 import math
+from typing import Annotated
 
 SELF_DIR_PATH = Path(__file__).parent
 PATTERN_DIR = SELF_DIR_PATH/"patterns"
 MAIN_MODEL = SELF_DIR_PATH/"main.xml"
 
+def generateCarProperties(carName: str,
+                          chassisLength: Annotated[float, "length in meters"],
+                          chassisWidth: Annotated[float, "width in meters"],
+                          chassisHeight: Annotated[float, "height in meters"],
+                          carMass: Annotated[float, "Car mass in kgs"] = 1500,
+                          wheelRadius: Annotated[float, "Car wheel/tire radius in meters"] = 0.5,
+                          wheelThickness:  Annotated[float, "wheel/tire thickness in meters"] = 0.2,
+                          wheelMass: Annotated[float, "wheel/tire mass in kgs"] = 30,
+                          wheelAxisSpacing: Annotated[float, "spacing of axis from center to edge, range: 0 to 1"] = 0.6,
+                          wheelSpacing: Annotated[float, "Distance between wheels, range: 0 to 1"] = 1,
+                          wheelMountHeight: Annotated[float, "range: -1 to 1"] = 0,
+                          lightsSpacing: Annotated[float, "range: 0 to 1"] = 0.6):
+    
+    chassisXSize = chassisLength/2
+    chassisYsize = chassisWidth/2
+    chassisZsize = chassisHeight/2
 
-def generate_random_coords(x, y):
+
+
+    prop = {
+        "car_name":f"{carName}",
+        "car_pos": "0 0 5",
+        "car_size": f"{chassisXSize} {chassisYsize} {chassisZsize}",
+        "car_mass": f"{carMass}",
+        
+        "car_wheel_size": f"{wheelRadius} {wheelThickness/2}",
+        "car_wheel_mass": f"{wheelMass}",
+        "left_front_wheel_pos": f"{chassisXSize*wheelAxisSpacing} {-chassisYsize*wheelSpacing} {chassisHeight*wheelMountHeight}",
+        "right_front_wheel_pos": f"{chassisXSize*wheelAxisSpacing} {chassisYsize*wheelSpacing} {chassisHeight*wheelMountHeight}",
+        "left_back_wheel_pos": f"{-chassisXSize*wheelAxisSpacing} {-chassisYsize*wheelSpacing} {chassisHeight*wheelMountHeight}",
+        "right_back_wheel_pos": f"{-chassisXSize*wheelAxisSpacing} {chassisYsize*wheelSpacing} {chassisHeight*wheelMountHeight}",
+
+        "car_front_lights_pos": f"{chassisXSize} 0 0",
+        "car_front_right_light_pos":f"0 {-chassisYsize*lightsSpacing} 0",
+        "car_front_left_light_pos":f"0 {chassisYsize*lightsSpacing} 0",
+
+    }
+
+
+    return prop
+
+def generateTopDownCamProperties(cameraName:str, xlen, ylen, camFov=90):
+    camHeight = calculateCameraHeight(xlen, ylen, camFov)
+    prop= {
+        "cam_name": f"{cameraName}",
+        "cam_pos": f"0 0 {camHeight}",
+        "cam_euler": "0 0 -90",
+        "cam_fov": f"{camFov}"
+    }
+    return prop
+
+
+def _generateRandomCoords(x, y):
     x_coord = random.uniform(-x / 2, x / 2)
     y_coord = random.uniform(-y / 2, y / 2)
     return (x_coord, y_coord)
 
-def getWallsProperties(name, x, y, h, t):
+def generateWallsProperties(name, xlen, ylen, hlen, tlen = 0.1):
+    x = xlen/2
+    y = ylen/2
+    h = hlen/2
+    t = tlen
     prop = {
         "ground_name": f"{name}",
         "board_size": f"{x} {y} {t}",
@@ -82,21 +138,17 @@ if __name__ == "__main__":
     generator = MujocoXMLGenerator()
 
 
-    BOARD_X = 10
-    BOARD_Y = 15
-    GROUND_PROP = getWallsProperties("main_ground", BOARD_X, BOARD_Y, 2, 0.1)
-    CAMERA_FOV = 45
-    CAMERA_Z = calculateCameraHeight(BOARD_X, BOARD_Y, CAMERA_FOV)
-    CAR_INIT_CORDS = generate_random_coords(BOARD_X, BOARD_Y)
+    BOARD_LENGTH = 20
+    BOARD_WIDTH = 20
+    BOARD_WALLS_HEIGHT = 2
+    GROUND_PROP = generateWallsProperties("main_ground", BOARD_LENGTH, BOARD_WIDTH, BOARD_WALLS_HEIGHT)
+    CAM_PROPERTIES = generateTopDownCamProperties("TopDownCam1", BOARD_LENGTH, BOARD_WIDTH, 45)
+    CAR_PROPERTIES = generateCarProperties("car1", 5, 2, 0.25)
     XML_ELEMENTS = [
         [generator.root, PATTERN_DIR/"boardAssetPattern.xml"],
         [generator.worldbodyNode, PATTERN_DIR/"boardBodyPattern.xml", GROUND_PROP],
-        [generator.worldbodyNode, PATTERN_DIR/"cameraPattern.xml", {"cam_name": "TopDownCam",
-                                                                    "cam_pos": f"0 0 {CAMERA_Z}",
-                                                                    "cam_euler": "0 0 -90",
-                                                                    "cam_fov": str(CAMERA_FOV)}],
-        [generator.worldbodyNode, PATTERN_DIR/"carPattern.xml", {"car_name":"car1",
-                                                                "car_pos":f"{CAR_INIT_CORDS[0]} {CAR_INIT_CORDS[1]} 0.5"}],                                              
+        [generator.worldbodyNode, PATTERN_DIR/"cameraPattern.xml", CAM_PROPERTIES],
+        [generator.worldbodyNode, PATTERN_DIR/"carPattern.xml", CAR_PROPERTIES],                                              
     ]
 
     for args in XML_ELEMENTS:

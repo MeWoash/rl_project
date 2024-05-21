80a1f80e306222a806c0d46d378b221d24f19892
Milosz Zubala <miloszzubala@gmail.com>
2024-05-21 15:58:18 +0200

    awd

--- src/PostProcessing/evaluation.py
+++ src/PostProcessing/evaluation.py
@@ -3,6 +3,7 @@ import sys
 import cv2
 import matplotlib
 from matplotlib.axes import Axes
+from matplotlib.colors import Normalize
 from matplotlib.figure import Figure
 import numpy as np
 import pandas as pd
@@ -25,10 +26,6 @@ from PathsConfig import *
 # autopep8: on
 
 
-
-def evaluate_reward_function(reward_function, reward_args, **kwargs):
-    reward, reward_info = reward_function(**reward_args)
-    return reward
     
 def prepare_data_for_reward_function():
     reward_params = {
@@ -49,10 +46,17 @@ def prepare_data_for_reward_function():
     angles = np.linspace(0, len(observation[OBS_INDEX.ANGLE_DIFF_BEGIN:OBS_INDEX.ANGLE_DIFF_END+1])*math.pi, 4)
     
     
-    for spawn_kwargs in CAR_SPAWN_KWARGS:
+    axs:list[list[Axes]]
+    fig:Figure
+    car_spawn_kwargs = CAR_SPAWN_KWARGS[:3]
+    
+    fig, axs = plt.subplots(len(car_spawn_kwargs), len(angles), figsize=(10,10))
+    norm = Normalize(vmin=0, vmax=1)
+    
+    for i, spawn_kwargs in enumerate(car_spawn_kwargs):
         spawn_point = np.array(spawn_kwargs['pos'][:2])
         init_car_distance = np.linalg.norm(spawn_point - parking_point)
-        for angle in angles:
+        for j, angle in enumerate(angles):
             rewards = []
             for xy in XY:
                 car_dist_to_parking = np.linalg.norm(xy - parking_point)
@@ -63,17 +67,25 @@ def prepare_data_for_reward_function():
                 reward, reward_info = calculate_reward(observation, init_car_distance, reward_params)
                 # print(reward)
                 rewards.append(reward)
+                
             rewards = np.array(rewards).reshape(mesh[0].shape)
             
-            plt.figure(figsize=(10, 8))
-            plt.pcolormesh(mesh[0], mesh[1], rewards, shading='auto')
-            plt.colorbar()
-            plt.xlabel('X Coordinate')
-            plt.ylabel('Y Coordinate')
-            plt.title(f'angle diff = {angle}')
-            plt.show()
-
+            im = axs[i, j].pcolormesh(mesh[0], mesh[1], rewards, shading='auto', norm=norm)
+            axs[i, j].scatter(*spawn_point, color="red", s=25)
+            # axs[i, j].scatter(*parking_point, color="white", s=25)
+            # axs[i, j].set_xlabel('X Coordinate')
+            # axs[i, j].set_ylabel('Y Coordinate')
+            axs[i, j].set_title(f'{angle:.2f}')
+            axs[i, j].set_aspect('equal')
     
+    
+    colorbar_ax = fig.add_axes([0.1, 0.05, 0.8, 0.02])
+    cbar = fig.colorbar(im, cax=colorbar_ax, orientation='horizontal')
+    cbar.set_label('Reward Value')
+    fig.subplots_adjust(left=0.1, right=0.9, top=0.95, bottom=0.1)
+    plt.show()
+    fig.savefig(str(Path(MEDIA_DIR,"reward_function.png")))
+
 
 if __name__ == "__main__":
     prepare_data_for_reward_function()
